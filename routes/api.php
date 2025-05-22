a44fd7f541ca821806717f7b13d4dd6559ae6440
WahyuTomo14 <wahyuxd14@gmail.com>
2025-05-22 22:03:47 +0700

    buat route api register

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
